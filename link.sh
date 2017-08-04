e666368aec1173e9ac77742b527f9c1d4d84e450
TatsuyaMiyado <t-miyado@sakura.ad.jp>
2017-08-04 09:35:10 +0900

    add link.sh

---
+++ link.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -sf ~/vscode-settings/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/vscode-settings/settings.json ~/Library/Application\ Support/Code/User/settings.json
